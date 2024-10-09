999cee74487aa18ff733e853ce69afe6a9a93289
Rico Erlangga <ricoerlanggaa@gmail.com>
2024-10-09 15:09:37 +0700

    add next-pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+import nextPWA from 'next-pwa'
 
-export default nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  compiler: {
+    removeConsole: process.env.NODE_ENV !== 'development'
+  }
+}
+
+const withPWA = nextPWA({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+  skipWaiting: true
+})
+
+export default withPWA(nextConfig)
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,24 @@
 import type { ReactNode } from 'react'
+import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import ReduxProvider from '@/providers/ReduxProvider'
 import ReactQueryProvider from '@/providers/ReactQueryProvider'
 import ThemeProvider from '@/providers/ThemeProvider'
 
 const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'PipeChat',
+  description:
+    'All-in-one business messaging platform uniting teams, conversations, data, and knowledge in one place.',
+  manifest: '/manifest.json',
+  viewport:
+    'minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover',
+  icons: [
+    { rel: 'apple-touch-icon', url: 'icons/icon-128x128.png' },
+    { rel: 'icon', url: 'icons/icon-128x128.png' }
+  ]
+}
 
 export default function RootLayout({
   children
